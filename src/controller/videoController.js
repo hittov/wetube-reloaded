f4543cc5896ec58a48e5618cfcff18f0e766bd4b
hitto <anothernoble@gmail.com>
2023-02-20 02:03:07 +0900

    Pug Conditionals and Iteration

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,7 +1,42 @@
 // pug 랜더링 res.render("home");
 // render명은 파일명과 같아야함 띄어쓰기 X 대문자 X
 // render는 2개의 argument 1. view 이름 2. 템플릿 변수 {변수: "value" 원하는 만큼}
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+// const fakeUser = {
+//   username: "pty",
+//   loggedIn: false,
+// };
+
+// Mixins는 똑똑한 partial이다.
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
+
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
